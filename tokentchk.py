d0e3689e856700017bbe27c479a5b0efb3ee0bc6
Fabio Andrijauskas <biozit@gmail.com>
2022-05-18 15:02:06 -0700

    tool checkmk

--- tokentchk.py
+++ tokentchk.py
@@ -8,12 +8,11 @@ import random
 import os
 from toolcheck import executeCommandBD
 from toolcheck import checkOuput
+import sys
 
 # private key used for the issuer
 with open("/root/scitoken/pkm.key", "r") as file_pointer:
         private_key_contents = file_pointer.read()
-
-
 
 loaded_private_key = serialization.load_pem_private_key(
         private_key_contents.encode(),
@@ -22,11 +21,7 @@ loaded_private_key = serialization.load_pem_private_key(
     )
 
 # file size in bytes
-size = 1024*1024;
-
-# caches list
-file_lcache = open("/root/scitoken/caches.txt", "r")
-lcache = lines = file_lcache.read().splitlines()
+size = 1024;
 
 # create the tokem
 token = scitokens.SciToken(loaded_private_key,key_id="071a")
@@ -55,19 +50,33 @@ else:
 	print("file on origin "+filename)
 
 
-# try to fetch the files from the caches
-for cache in lcache:
 
-	token.update_claims({"scope":"read:/ospool/monitoring/PROTECTED/"});
-	token.update_claims({"aud":"ANY"});
-	token.update_claims({"ver":"scitoken:2.0"});
-	token.update_claims({"sub":"osgmon"});
+token.update_claims({"scope":"read:/ospool/monitoring/PROTECTED/"});
+token.update_claims({"aud":"ANY"});
+token.update_claims({"ver":"scitoken:2.0"});
+token.update_claims({"sub":"osgmon"});
 
-	e = executeCommandBD("curl -i  -H 'Authorization: Bearer " +serialized_token.decode()+"' -k "+cache+":8444/ospool/monitoring/PROTECTED/"+ filename)
-	resp = checkOuput(e)
-	if (resp != 200):
-		print("Error reading cache "+resp) 
-	else:
-		print("file on origin "+filename)
+e = executeCommandBD("curl -i  -H 'Authorization: Bearer " +serialized_token.decode()+"' -k "+sys.argv[1]+":8444/ospool/monitoring/PROTECTED/"+ filename)
+resp = checkOuput(e)
+if (resp != 200):
+	print("Error reading cache "+resp) 
+else:
+	print("file on origin "+filename)
 
+
+executeCommandBD("rm "+filepath)
+
+token.update_claims({"scope":"write:/"});
+token.update_claims({"aud":"ANY"});
+token.update_claims({"ver":"scitoken:2.0"});
+token.update_claims({"sub":"osgmon"});
+
+serialized_token = token.serialize(issuer="https://osg-htc.org/monitoring")
+
+e = executeCommandBD("curl -i -X DELETE  -H 'Authorization: Bearer " +serialized_token.decode()+"' -k https://stash-xrd.osgconnect.net:1094/ospool/monitoring/PROTECTED/"+filepath)
+resp = checkOuput(e)
+if (resp != "200"):
+	print("Error delete origin "+resp) 
+else:
+	print("file delete origin "+filename)
 
--- toolcheck.py
+++ toolcheck.py
@@ -1,22 +1,25 @@
 import subprocess
 
 def executeCommandBD(com):
-        with open('out1.txt','w+') as fout:
-                with open('err1.txt','w+') as ferr:
-                        out=subprocess.call([com],stdout=fout,stderr=ferr,timeout=500,shell=True)
-                        fout.seek(0)
-                        output=fout.read()
-                        ferr.seek(0) 
-                        errors = ferr.read()
-                        return output
+	with open('out1.txt','w+') as fout:
+		with open('err1.txt','w+') as ferr:
+			print(com)
+			out=subprocess.call([com],stdout=fout,stderr=ferr,timeout=500,shell=True)
+			fout.seek(0)
+			output=fout.read()
+			ferr.seek(0) 
+			errors = ferr.read()
+			return output
 
 def checkOuput(data):
-        try:
-                lines = data.splitlines()
-                for l in lines:
-                        if ( l.find('HTTP/1.1') != -1 ):
-                                data = l.split(' ')
-                                return (data[1]);
-        except:
-                return "204"
+	try:
+		lines = data.splitlines()
+		for l in lines:
+			if ( l.find('HTTP/1.1') != -1 ):
+				data = l.split(' ')
+				if((data[1] != "100") and (data[1] != "200")):
+					return "204"
+		return "200"
+	except:
+		return "204"
 
